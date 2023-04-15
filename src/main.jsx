e70731e73c740738396eab91c55a261b3e1a39e8
milanpoudel <milanwebdeveloper1@gmail.com>
2023-04-15 20:36:58 +0545

    fix/fixed few issues with context provider 1

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import UnitContextProvider from "./components/Context/UnitContextProvider";
+import UnitContextProvider from "./components/Context/unitContextProvider";
 
 ReactDOM.render(
   <React.StrictMode>
